f5f3c65f03d3e5297c56d899aed31d12b082510c
syrayse <ruipedronetoreis12@gmail.com>
2020-05-20 21:33:58 +0100

    dispatcher fully working

--- src/Cliente.c
+++ src/Cliente.c
@@ -1,22 +1,29 @@
 #include "Common.h"
 #include "Request.h"
+#include <time.h>
+#include <stdio.h>
 
 int main() {
+  //srand(time(NULL));
   int fd1 = open(CL_TO_SR_PIPE, O_WRONLY);
-  int fd2 = open(SR_TO_CL_PIPE, O_RDONLY);
+
   ssize_t n, d;
-  char buff[MAX_BUFFER_SIZE];
 
-  Request r = malloc(sizeof(struct request));
-  r->ID = 123;
-  r->nArgs = 2;
-  r->argv = malloc(sizeof(char*)*2);
-  r->argv[0] = strdup("lol1");
-  r->argv[1] = strdup("lol2");
+  while(1) {
+    Request r = malloc(sizeof(struct request));
+    r->ID = rand() % (LIST_HISTORY + 1);
+    r->nArgs = 2;
+    r->argv = malloc(sizeof(char*)*2);
+    r->argv[0] = strdup("lol1");
+    r->argv[1] = strdup("lol2");
 
-  char * buffer = serialize_request(r, &n);
+    char * buffer = serialize_request(r, &n);
 
-  write(fd1, buffer, n);
+    write(fd1, buffer, n);
+
+    free(buffer);
+    sleep(1);
+  }
 
   /*
   if( !fork() ) {
--- src/Servidor.c
+++ src/Servidor.c
@@ -2,20 +2,80 @@
 #include "Request.h"
 #include <stdio.h>
 
-int main() {
-  int i;
-  int fd1 = open(CL_TO_SR_PIPE, O_RDONLY);
-  int fd2 = open(SR_TO_CL_PIPE, O_WRONLY);
-  ssize_t n;
-  char buff[MAX_BUFFER_SIZE];
+typedef void (*DispatchFunc)(char**);
 
+void process_pipe_timeout(char** argv) {
+  //g_pipe_timeout = atoi(argv[0]);
+  printf("pipe timeout\n");
+}
+
+void process_exec_timeout(char** argv) {
+  //g_exec_timeout = atoi(argv[0]);
+  printf("exec timeout\n");
+}
+void process_exec_task(char** argv) {
+  // Devera executar um novo processo
+  // Novo processo serializa task e comunica o respectivo id.
+  printf("exec task\n");
+}
+
+void process_list_execs(char** argv) {
+  // Criar novo processo
+  // Processo comunica tasks em aberto.
+  printf("list execs\n");
+}
+
+void process_kill_task(char** argv) {
+  // Envia um SIGKILL a uma task.
+  // Assinala comando como COMMAND_TERMINATED
+  printf("kill task\n");
+}
+
+void process_list_history(char** argv) {
+  // Faz dump do processo de historico.
+  printf("list history");
+}
+
+void setup_dispatcher(DispatchFunc *v) {
+  v[SET_PIPE_TIMEOUT] = process_pipe_timeout;
+  v[SET_EXEC_TIMEOUT] = process_exec_timeout;
+  v[EXECUTE_TASK] = process_exec_task;
+  v[LIST_IN_EXECUTION] = process_list_execs;
+  v[TERMINATE_TASK] = process_kill_task;
+  v[LIST_HISTORY] = process_list_history;
+}
+
+
+int main() {
+  int i, fd1 = open(CL_TO_SR_PIPE, O_RDONLY);;
+  ssize_t n;
+  Request r;
+  char buffer[MAX_BUFFER_SIZE];
+  DispatchFunc req_dispatch[LIST_HISTORY + 1];
+  setup_dispatcher(req_dispatch);
+
+  while( (n = read(fd1, buffer, MAX_BUFFER_SIZE)) > 0 ) {
+    r = deserialize_request(buffer, n);
+
+    printf("##################\n");
+    printf("ID:%d\nnArgs:%d\n",r->ID, r->nArgs);
+    for(i = 0; i < r->nArgs; i++)
+      printf("\tword %d: %s\n", i, r->argv[i]);
+    (*req_dispatch[r->ID])(r->argv);
+    printf("##################\n");
+
+    free(r);
+  }
+
+  /*
   n = read(fd1, buff, MAX_BUFFER_SIZE);
-  Request r = deserialize_request(buff, n);
+
+
 
   printf("ID:%d\nnArgs:%d\n",r->ID, r->nArgs);
   for(i = 0; i < r->nArgs; i++)
     printf("\tword %d: %s\n", i, r->argv[i]);
-
+  */
   //write()
 
 
