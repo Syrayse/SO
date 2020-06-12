923bf04c7e5a62b1dc3052839897d05c233e350c
syrayse <ruipedronetoreis12@gmail.com>
2020-06-12 21:06:27 +0100

    more info to povoamento

--- src/povoamento.sh
+++ src/povoamento.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # todos os comandos.
-commands=("ls -l" "cat /etc/passwd" "este_comand_nao_existe" "cat /etc/passwd | head -5 | sort" "sleep 5" "sleep 10" "sleep 15" "sleep 25" "sleep 30" "ls | sleep 1 | cat" "ls | sleep 5 | cat" "ls | sleep 10 | cat" "ls | sleep 15 | cat")
+commands=("ls -l" "cat /etc/passwd" "este_comand_nao_existe" "cat /etc/passwd | head -5 | sort" "grep -v ^# /etc/passwd | cut -f7 -d: | uniq | wc -l" "sleep 15" "sleep 25" "sleep 30" "ls /etc | wc -l" "ls | sleep 5 | cat" "ls | sleep 10 | cat" "ls | sleep 15 | cat")
 
 # A semente.
 RANDOM=$$$(date +%s)
